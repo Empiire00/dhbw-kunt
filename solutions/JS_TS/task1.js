15a332ad7a6494676115cff25d4f8f65faa987b4
Empiire00 <tobiaskreuss@hotmail.de>
2021-11-03 16:54:29 +0100

    feat: added decoding functionality

--- solutions/JS_TS/task1.js
+++ solutions/JS_TS/task1.js
@@ -1,17 +1,65 @@
 var bitstring = "0110001";
-hammingEncode(bitstring);
-function hammingEncode(input) {
-    var result = "";
-    var inputArr = input.split("").map(function (a) { return parseInt(a); });
-    var outArr;
-    var index = 1;
-    inputArr.forEach(function (bit) {
-        while (index % 2 != 0) {
-            index++;
+var bitstringToDecode = "00011110000";
+function hammingEncode_7_4(inp) {
+    var arr = inp.split("").map(function (str) { return parseInt(str); });
+    var outArr = [];
+    var parityPos = [];
+    // calc bit positions
+    var highestExponent = Math.ceil(Math.log2(inp.length));
+    for (var i = 0; i <= highestExponent; i++) {
+        parityPos.push(Math.pow(2, i));
+    }
+    // fill other positions
+    var lastWrittenPos = 0;
+    var _loop_1 = function (i) {
+        if (!(parityPos.includes(i + 1))) {
+            outArr[i] = arr[lastWrittenPos];
+            // update parity bits
+            parityPos.forEach(function (pos) {
+                var _a;
+                if (((i + 1) & (pos)) > 0) {
+                    outArr[pos - 1] = (((_a = outArr[pos - 1]) !== null && _a !== void 0 ? _a : 0) + arr[lastWrittenPos]) % 2;
+                }
+            });
+            lastWrittenPos++;
         }
-        outArr[index] = bit;
-        index++;
-    });
-    console.log(outArr);
-    return result;
+    };
+    for (var i = 0; i < 11; i++) {
+        _loop_1(i);
+    }
+    return outArr.join("");
 }
+function hammingDecode_7_4(inp) {
+    var _a;
+    var arr = inp.split("").map(function (str) { return parseInt(str); });
+    var parityPos = [];
+    var parityBits = [];
+    // calc bit positions
+    var highestExponent = Math.ceil(Math.log2(inp.length));
+    for (var i = 0; i <= highestExponent; i++) {
+        parityPos.push(Math.pow(2, i));
+    }
+    // fill other positions
+    for (var i = 0; i < inp.length; i++) {
+        // update parity bits
+        for (var index = 0; index < parityPos.length; index++) {
+            var pos = parityPos[index];
+            if (((i + 1) & (pos)) > 0) {
+                parityBits[index] = (((_a = parityBits[index]) !== null && _a !== void 0 ? _a : 0) + arr[i]) % 2;
+            }
+        }
+    }
+    // calculate wrong bit
+    var wrongPosition = parityBits.reduce(function (a, b, index) {
+        return a + b * Math.pow(2, index);
+    });
+    // correct wrong bit
+    if (wrongPosition) {
+        arr[wrongPosition - 1] = Math.abs(arr[wrongPosition - 1] - 1);
+    }
+    console.log(wrongPosition);
+    return arr.join("");
+}
+console.log(hammingDecode_7_4(bitstringToDecode));
+// console.log("bitstring : '" + bitstring + "'");
+// console.log("hamming encoded code: '" + hammingEncode_7_4(bitstring) + "'");
--- solutions/JS_TS/task1.ts
+++ solutions/JS_TS/task1.ts
@@ -1,14 +1,15 @@
-const bitstring: string = "0110001"
+const bitstring: string = "0110001";
+const bitstringToDecode: string = "00011110000";
 
 function hammingEncode_7_4(inp: string): string {
     const arr: number[] = inp.split("").map(str => parseInt(str));
     const outArr: number[] = [];
-    const parityPos: number[] = []
+    const parityPos: number[] = [];
 
     // calc bit positions
     const highestExponent: number = Math.ceil(Math.log2(inp.length));
     for (let i: number = 0; i <= highestExponent; i++) {
-        parityPos.push(2 ** i)
+        parityPos.push(2 ** i);
     }
 
     // fill other positions
@@ -18,8 +19,8 @@ function hammingEncode_7_4(inp: string): string {
             outArr[i] = arr[lastWrittenPos];
             // update parity bits
             parityPos.forEach(pos => {
-                if (((i +1 ) & (pos)) > 0) {
-                    outArr[pos -1 ] = ((outArr[pos -1] ?? 0) + arr[lastWrittenPos]) % 2
+                if (((i + 1) & (pos)) > 0) {
+                    outArr[pos - 1] = ((outArr[pos - 1] ?? 0) + arr[lastWrittenPos]) % 2;
                 }
             })
             lastWrittenPos++;
@@ -27,5 +28,44 @@ function hammingEncode_7_4(inp: string): string {
     }
     return outArr.join("");
 }
+function hammingDecode_7_4(inp: string): string {
+    let arr: number[] = inp.split("").map(str => parseInt(str));
+    const parityPos: number[] = [];
+    const parityBits: number[] = [];
+
+    // calc bit positions
+    const highestExponent: number = Math.ceil(Math.log2(inp.length));
+    for (let i: number = 0; i <= highestExponent; i++) {
+        parityPos.push(2 ** i);
+    }
+
+    // fill other positions
+    for (let i: number = 0; i < inp.length; i++) {
+        // update parity bits
+        for (let index: number = 0; index < parityPos.length; index++) {
+            let pos = parityPos[index];
+            if (((i + 1) & (pos)) > 0) {
+                parityBits[index] = ((parityBits[index] ?? 0) + arr[i]) % 2;
+            }
+        }
+    }
+    // calculate wrong bit
+    const wrongPosition = parityBits.reduce((a, b, index) => {
+        return a + b * Math.pow(2, index);
+    })
+    // correct wrong bit
+    if (wrongPosition) {
+        arr[wrongPosition - 1] = Math.abs(arr[wrongPosition - 1] - 1);
+    }
+    // get data bits
+    arr = arr.filter((val, ind)=>{
+        return !parityPos.includes(ind + 1)
+    })
+    return arr.join("");
+}
+console.info("\t --ENCODING--");
 console.log("bitstring : '" + bitstring + "'");
-console.log("hamming encoded code: '" + hammingEncode_7_4(bitstring) + "'");
+console.log("hamming encoded bits: '" + hammingEncode_7_4(bitstring) + "'");
+console.info("\t --DECODING--");
+console.log("hamming encoded bits: '" + bitstringToDecode + "'");
+console.log("hamming decoded bits: '" + hammingDecode_7_4(bitstringToDecode) + "'");
